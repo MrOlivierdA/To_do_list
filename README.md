3e5481fcef2556fbb90bb5cfd1b26444920bd819
Mr.Olivier.dA <99800936+MrOlivierdA@users.noreply.github.com>
2023-08-18 07:26:21 +0200

    mise à jour up

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# To_do_list
+# ToDo list
+
+### Projet 2023
+
+✨**Olivier Dardenne**✨
+
+_Jr Web Dev student_
+
+Ce projet m'a permis de refaire uns synthesede cette première partie de formation touchant du html au css et javascript tout en devant reprendre le temps de appronfondir l'a,,alyse de la logique et coherance du code dans son entièreté.
--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-const form = document.getElementsByTagName("form")[0];
-const output = document.getElementsByTagName("ul")[0];
+const form = document.querySelector("form");
 
-function addTask(e) {
-  e.preventDefault();
-  const taskValue = form[0].value;
-  output.innerHTML = `<li>
-    <p>${taskValue}</p>
-    <div>
-      <button class="done"></button>
-      <button class="delete">Delete</button>
-    </div>
-  </li>`;
+// Stocker la tache
+function storeList() {
+  window.localStorage.todoList = list.innerHTML;
 }
-form.addEventListener("submit", addTask);
+
+function getTodos() {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li>Cliquez sur une tâche pour la supprimer</li>`;
+  }
+}
+
+window.addEventListener("load", getTodos);
+
+// Ajouter un element
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  list.innerHTML += `<li>${item.value}</li>`;
+  item.value = "";
+  storeList();
+});
+
+// Enlever element
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
+  storeList();
+});
